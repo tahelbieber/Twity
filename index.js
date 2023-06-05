7323d9af0b42dc8e6146421afbc041ca970a4e11
מרקו סוויסה - עוזר ראש העיר <marco-s@askelonmuni.local>
2023-06-05 09:38:40 +0300

    new

--- index.js
+++ index.js
@@ -1,59 +1,74 @@
+const loginUsernameInput = document.getElementById("login-username");
+const loginPasswordInput = document.getElementById("login-password");
+const registerUsernameInput = document.getElementById("register-username");
+const registerPasswordInput = document.getElementById("register-password");
+const loginContainer = document.getElementById("login-container");
+const registerContainer = document.getElementById("register-container");
+const homeContainer = document.getElementById("home-container");
+const tweetsContainer = document.getElementById("tweets-container");
+const tweetTextInput = document.getElementById("tweet-text");
 
 function login() {
-  var username = document.getElementById("login-username").value;
-  var password = document.getElementById("login-password").value;
+  const username = loginUsernameInput.value;
+  const password = loginPasswordInput.value;
 
- 
-  if (username === "dana" && password === "סיסמה") {
-      document.getElementById("login-container").style.display = "none";
-      document.getElementById("register-container").style.display = "none";
-      document.getElementById("home-container").style.display = "block";
+  const storedUsername = localStorage.getItem("username");
+  const storedPassword = localStorage.getItem("password");
+
+  if (username === storedUsername && password === storedPassword) {
+    loginContainer.style.display = "none";
+    registerContainer.style.display = "none";
+    homeContainer.style.display = "block";
   } else {
-      alert("שם משתמש או סיסמה שגויים");
+    alert("שם משתמש או סיסמה שגויים");
   }
 }
+
 function register() {
-  var username = document.getElementById("register-username").value;
-  var password = document.getElementById("register-password").value;
+  const username = registerUsernameInput.value;
+  const password = registerPasswordInput.value;
 
-  
   if (username === "dana") {
-      
-      localStorage.setItem("username", username);
-      localStorage.setItem("password", password);
-      document.getElementById("login-container").style.display = "none";
-      document.getElementById("register-container").style.display = "none";
-      document.getElementById("home-container").style.display = "block";
+    localStorage.setItem("username", username);
+    localStorage.setItem("password", password);
+    loginContainer.style.display = "none";
+    registerContainer.style.display = "none";
+    homeContainer.style.display = "block";
   } else {
-      alert("שם משתמש שגוי");
+    alert("שם משתמש שגוי");
   }
 }
+
 function showLoginForm() {
-  document.getElementById("login-container").style.display = "block";
-  document.getElementById("register-container").style.display = "none";
-  document.getElementById("home-container").style.display = "none";
+  showForm(loginContainer);
 }
 
 function showRegisterForm() {
-  document.getElementById("login-container").style.display = "none";
-  document.getElementById("register-container").style.display = "block";
-  document.getElementById("home-container").style.display = "none";
+  showForm(registerContainer);
+}
+
+function showForm(formContainer) {
+  loginContainer.style.display = "none";
+  registerContainer.style.display = "none";
+  homeContainer.style.display = "none";
+  formContainer.style.display = "block";
 }
 
 function addTweet() {
-  var tweetText = document.getElementById("tweet-text").value;
-  var currentDatetime = new Date().toLocaleString();
+  const tweetText = tweetTextInput.value;
+  const currentDatetime = new Date().toLocaleString();
+
   if (tweetText.trim() !== "") {
+    const tweetDiv = document.createElement("div");
+    tweetDiv.className = "tweet";
+    tweetDiv.innerHTML = "<p>" + tweetText + "</p><span>" + currentDatetime + "</span>";
 
-      var tweetDiv = document.createElement("div");
-      tweetDiv.className = "tweet";
-      tweetDiv.innerHTML = "<p>" + tweetText + "</p><span>" + currentDatetime + "</span>";
+    tweetsContainer.appendChild(tweetDiv);
 
-      document.getElementById("tweets-container").appendChild(tweetDiv);
-
-      document.getElementById("tweet-text").value = "";
+    tweetTextInput.value = "";
   }
 }
+
 
 
 
